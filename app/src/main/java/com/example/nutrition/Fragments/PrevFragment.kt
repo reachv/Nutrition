8c839112a7249f9f9487331368bf71e76a8bd24e
reachv <reach.vann@gmail.com>
2024-01-16 16:22:29 -0800

    Frontend Design: PieChart Implementation

---
+++ app/src/main/java/com/example/nutrition/Fragments/PrevFragment.kt
@@ -0,0 +1,29 @@
+package com.example.nutrition.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nutrition.R
+
+class PrevFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_prev, container, false)
+
+
+
+        // Inflate the layout for this fragment
+        return view
+    }
+}
